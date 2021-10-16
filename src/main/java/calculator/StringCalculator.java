a31c1d5ee2450b2b01c1e3a5eca5be5ae5b446c9
Asawari Waghmare <asawari.waghmare26@gmail.com>
2021-10-17 00:57:16 +0530

    numbers delimited by comma should be summed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,10 +3,21 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	 String[] numbers = input.split(",");
     	if (input.isEmpty()) {
             return 0;
+        } else if (numbers.length > 1) {
+            return getSum(numbers);
         }
-        return Integer.parseInt(input);
+    	 return getInt(input);
+    }
+    
+    private int getSum(String[] numbers) {
+        return getInt(numbers[0]) + getInt(numbers[1]);
+    }
+
+    private int getInt(String number) {
+        return Integer.parseInt(number);
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -5,6 +5,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
+	
+	private StringCalculator calculator;
+
+    @Before
+    public void initialize() {
+        calculator = new StringCalculator();
+    }
+
 
     @Test
     void empty_string_should_return_0() {
@@ -16,5 +24,10 @@ class StringCalculatorShould {
     void string_with_single_number_should_return_number_as_int() {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
+    }
+    
+    @Test
+    public void numbersCommaDelimitedShouldBeSummed() {
+        assertEquals(calculator.add("1,2"), 3);
     }
 }
